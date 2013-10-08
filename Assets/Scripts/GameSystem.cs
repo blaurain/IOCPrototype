cb8124aad2de658bb7bad8ecd13c13c36b6a6766
Billy Laurain <williamlaurain@gmail.com>
2013-10-07 21:08:52 -0400

    added a prefab, script and scene to build structure

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSystem : MonoBehaviour
+{
+    #region Singleton
+    private static GameSystem instance;
+    public static GameSystem Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (GameSystem)GameObject.FindObjectOfType(typeof(GameSystem));
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    public void Awake()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+}
